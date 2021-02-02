ee0012ec96824a927af3a6452f2353059b83b0fd
sriharsha28064212 <78369194+sriharsha28064212@users.noreply.github.com>
2021-02-02 12:05:03 +0530

    Update basic switchpin led.ino

--- basic switchpin led.ino
+++ basic switchpin led.ino
@@ -1,1 +1,18 @@
-#demo
+int ledpin=13,switchpin=8;
+void setup()
+{
+  pinMode(switchpin, INPUT);
+  pinMode(ledpin,OUTPUT);
+}
+
+void loop()
+{
+  if(digitalRead(switchpin)==HIGH)
+     {
+       digitalWrite(ledpin, HIGH);
+     }
+  else
+     {
+       digitalWrite(ledpin, LOW);
+     }
+}    
