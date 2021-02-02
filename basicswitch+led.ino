73d53852ad654b26713d7df58722526fc54228ac
Sri Harsha <sai.sriharsha2020@vitstudent.ac.in>
2021-02-02 12:13:46 +0530

    rename

---
+++ basicswitch+led.ino
@@ -0,0 +1,19 @@
+int ledpin=13,switchpin=8;
+void setup()
+{
+  pinMode(switchpin, INPUT);
+  pinMode(ledpin,OUTPUT);
+}
+
+void loop()
+{
+  if(digitalRead(switchpin)==HIGH)
+     {
+       digitalWrite(ledpin, HIGH);
+     }
+  else
+     {
+       digitalWrite(ledpin, LOW);
+     }
+}  
+
