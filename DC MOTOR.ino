0e920b2ba03c645ce7a6bb7800f655a9c38f9648
Sri_Harsha <78369194+sriharsha28064212@users.noreply.github.com>
2021-02-23 22:55:06 +0530

    dc motor.ino

---
+++ DC MOTOR.ino
@@ -0,0 +1,22 @@
+int motorPin=9;
+void setup()
+{
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop()
+{
+  for (int i = 0; i<=255; i++)
+  {
+    analogWrite(motorPin,i);
+    delay(50);
+  }
+ delay(1000);
+  
+  for(int i=255; i>=0; i--)
+  {
+    analogWrite(motorPin,i);
+    delay(50);
+  }
+ delay(1000);
+}
